bf12d246a282e7b9b1a6ca774b34dbe684cd8032
Everson Antonio da Silva <eversonantoniodasilva@gmail.com>
2019-09-24 09:43:16 -0400

    Implementação parcial do algoritmo Q-Leaning feita em sala.
    Signed-off-by: Everson Antonio da Silva <eversonantoniodasilva@gmail.com>

--- Q-Learning/src/q/learning/Posicao.java
+++ Q-Learning/src/q/learning/Posicao.java
@@ -11,14 +11,17 @@ package q.learning;
  */
 public class Posicao {
     
-    int posicao_linha;
-    int posicao_coluna;
+    private int posicao_linha;
+    private int posicao_coluna;
 
-   
-    
     public Posicao(int posicao_linha, int posicao_coluna) {
         this.posicao_linha = posicao_linha;
         this.posicao_coluna = posicao_coluna;
+    }
+    
+    public void movePosicao(Posicao p, int[] a) {
+        this.posicao_linha = p.getPosicao_linha() + a[0];
+        this.posicao_coluna = p.getPosicao_coluna() + a[1];
     }
 
     
@@ -46,5 +49,9 @@ public class Posicao {
         return false;
     }
   
+    public void setPosicao(Posicao p){
+        this.posicao_coluna = p.posicao_coluna;
+        this.posicao_linha = p.posicao_linha;
+    }
      
 }
--- Q-Learning/src/q/learning/QLearning.java
+++ Q-Learning/src/q/learning/QLearning.java
@@ -10,33 +10,59 @@ package q.learning;
  * @author asafb
  */
 public class QLearning {
+    
+    double[][][] Q;
+    
+    int[][] matrizDeMovimento = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    double alfa = 0.25;
+    double gamma = 0.9;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        
-        //Onde estiver 3 eh obstaculo
-        
-        int[][] ambiente = new int[40][40];
-               
+        new QLearning().run();
 
-        Posicao posicaoAtual = new Posicao(0, 0);
+    }
+    
+    public void run(){
+        Posicao s = new Posicao(0, 0);
         Posicao posicaoFinal = new Posicao(32, 13);
+        //s1 = s'
+        Posicao s1 = new Posicao(0, 0);
 
-        double[][] matrizDeEstados = new double[8][4];
+        Q = new double[40][40][4];
 
-        int[][] matrizDeMovimento = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!s.equals(posicaoFinal)) {
         
-        while (!posicaoAtual.equals(posicaoFinal)) {
-        
+            int a = escolheAcao(s, 1);
             
-            
-            
+            s1.movePosicao(s, matrizDeMovimento[a]);
+            int r = recompensa(s1);
+            atualizaTabelaQ(s, a, s1, r);
+            if (r!=-1){
+                
+                s.setPosicao(s1);
+            }
+        }
+    }
+
+    private int escolheAcao(Posicao posicaoAtual, int epsilon) {
+        double[] vet = Q[posicaoAtual.getPosicao_linha()][posicaoAtual.getPosicao_coluna()];
+        int maior = 0;
+        for (int i = 0; i < vet.length; i++) {
             
         }
+        return maior;
+    }
 
+    private void atualizaTabelaQ(Posicao s, int a, Posicao s1, int r) {
+        int a1 = escolheAcao(s1, 1);
+        double maxQt = Q[s1.getPosicao_linha()][s1.getPosicao_coluna()][a1];
+        double[] Qs = Q[s.getPosicao_linha()][s.getPosicao_coluna()];
+        Qs[a] += alfa*(r+gamma*maxQt-Qs[a]);
     }
     
   
